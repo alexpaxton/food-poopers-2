b911fca328ff3f2f940977e664278fe1f17de1dd
alexpaxton <thealexpaxton@gmail.com>
2026-02-10 16:10:31 -0700

    Add Inter font and base styles to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,22 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
+import { Inter } from 'next/font/google';
 import StyledComponentsRegistry from '@/lib/registry';
 import AuthSessionProvider from '@/lib/session-provider';
 import './globals.css';
 
+const inter = Inter({ subsets: ['latin'] });
+
 export const metadata: Metadata = {
-  title: 'food-poopers-2',
+  title: 'Food Poopers',
   description: 'A Next.js application',
 };
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  userScalable: false,
+  maximumScale: 1,
+  width: 'device-width',
+}
 
 export default function RootLayout({
   children,
@@ -14,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <StyledComponentsRegistry>
           <AuthSessionProvider>{children}</AuthSessionProvider>
