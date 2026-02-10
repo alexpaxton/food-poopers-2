40ae1e47bbf476eb5e2a0e07f93b5b5665809684
alexpaxton <thealexpaxton@gmail.com>
2026-02-10 16:59:03 -0700

    Implement react query wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import StyledComponentsRegistry from '@/lib/registry';
 import AuthSessionProvider from '@/lib/session-provider';
+import { QueryProvider } from '@/lib/query-provider';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -27,7 +28,9 @@ export default function RootLayout({
     <html lang="en" className={inter.className}>
       <body>
         <StyledComponentsRegistry>
-          <AuthSessionProvider>{children}</AuthSessionProvider>
+          <AuthSessionProvider>
+            <QueryProvider>{children}</QueryProvider>
+          </AuthSessionProvider>
         </StyledComponentsRegistry>
       </body>
     </html>
